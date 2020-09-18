4452f6d45077863f48761c4ae11d25dd6baa1746
LennWi <70192362+LennWi@users.noreply.github.com>
2020-09-18 12:51:47 +0200

    Projektdateien hinzufügen.

---
+++ Uebung_HSP_Zahlenarray/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uebung_HSP_Zahlenarray
+{
+    class Program
+    {
+        static void Main()
+        {
+            int arrayGroesse = 0;
+
+            Console.Write("Wie viele Zahlen möchten sie eingeben? ");
+            arrayGroesse = Convert.ToInt32(Console.ReadLine());
+
+            double[] quadratzahlen = new double[arrayGroesse];
+
+            for(int i = 0; i < arrayGroesse; i++)
+            {
+                Console.Write("Geben Sie die " + (i + 1) + ". Zahl ein: ");
+                quadratzahlen[i] = Math.Pow(Convert.ToDouble(Console.ReadLine()),2);
+            }
+
+            Console.WriteLine("Berechne Quadratzahlen...");
+
+            for(int i = 0; i < arrayGroesse; i++)
+            {
+                Console.WriteLine("Quadrat der " + (i + 1) + ". Zahl: " + quadratzahlen[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
